e880664c9ec541a7d37a99979cb114915d9ebe57
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-03-02 09:02:17 +0600

    Toggle no phone found message based on search result

--- js/app.js
+++ js/app.js
@@ -10,6 +10,20 @@ const displayPhones = phones => {
     console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.innerText="";
+
+    //display 10 phones in a page
+    phones=phones.slice(0,10);
+
+    //display if no phone found
+    const noPhone = document.getElementById('no-found-msg');
+    if(phones.length===0){
+        noPhone.classList.remove('d-none');
+    }else{
+        noPhone.classList.add('d-none');
+    }
+
+
+    //display all phones
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -18,8 +32,7 @@ const displayPhones = phones => {
                         <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${phone.phone_name}</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque laudantium quod consequuntur repellat, fugiat ad.</p>
                         </div>
                     </div>
         `
@@ -35,4 +48,4 @@ document.getElementById('btn-search').addEventListener('click',function(){
 })
 
 
-loadPhone("phone");
+loadPhone("apple");
